d3890e921118a47e400a7f7ccb4625753b25288e
Sumitha2502 <sumitha0996@gmail.com>
2024-06-21 16:34:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import "./App.css";
+import "/node_modules/bootstrap/dist/css/bootstrap.css";
+import ShopList from "./ShopList";
+import ShopName from "./ShopName";
+import Card from "./Card";
+import Copyright from "./Copyright";
+
+function App() {
+  let [cart, setCart] = useState(0);
+  let datas = [
+    {
+      name: "Fancy Product",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: "$40.00 - $80.00",
+      sale: false,
+      originalPrice: "",
+      original: false,
+      reviews: false,
+    },
+    {
+      name: "Special Item",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: " $18.00",
+      sale: true,
+      originalPrice: "$20.00",
+      original: true,
+      reviews: true,
+    },
+    {
+      name: "Sale Item",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: "$25.00",
+      sale: true,
+      originalPrice: "",
+      original: false,
+      reviews: false,
+    },
+    {
+      name: "Popular Item",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: "$40.00",
+      sale: false,
+      originalPrice: "",
+      original: false,
+      reviews: true,
+    },
+    {
+      name: "Sale Item",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: " $25.00",
+      sale: true,
+      originalPrice: "$50.00",
+      original: true,
+      reviews: false,
+    },
+    {
+      name: "Fancy Product",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: " $120.00",
+      sale: false,
+      originalPrice: "$280.00",
+      original: true,
+      reviews: false,
+    },
+    {
+      name: "Special Item",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: " $18.00",
+      sale: true,
+      originalPrice: "$20.00",
+      original: true,
+      reviews: true,
+    },
+    {
+      name: "Special Item",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: " $20.00",
+      sale: false,
+      originalPrice: "",
+      original: false,
+      reviews: true,
+    },
+  ];
+
+  return (
+    <>
+      <ShopList cart={cart} setCart={setCart} />
+      <ShopName />
+      <section className="py-5">
+        <div className="container px-4 px-lg-5 mt-5">
+          <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            {datas.map((product, i) => {
+              return (
+                <Card product={product} key={i} cart={cart} setCart={setCart} />
+              );
+            })}
+          </div>
+        </div>
+      </section>
+      <Copyright />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Copyright.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Copyright() {
+  return (
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">
+          Copyright &copy; Your Website 2023
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Copyright;
